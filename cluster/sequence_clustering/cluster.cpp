5d0e7a54215b6d17ef10465233c2375446adf5f8
Michael Zhang <michael13162@gmail.com>
2018-05-26 22:45:34 +0800

    changed cluster.cpp to use blast_query

--- cluster/sequence_clustering/cluster.cpp
+++ cluster/sequence_clustering/cluster.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 #include "disjoint_set.cpp"
+#include "blast_query.cpp"
 
 using namespace std;
 
-const int NUM_WORKERS = 9;
-const int NUM_RANGES = 3;
-const float DISTANCE_THRESHOLD = .003125;
+const int NUM_WORKERS = 8;
+const float DISTANCE_THRESHOLD = .005;
 
 struct partition {
-  pair<int, int> first_partition;
-  pair<int, int> second_partition;
-  int thread_id;
+  int low;
+  int high;
+  string thread_id;
 };
 
 vector<string> uris;
+map<string, int> uri_ids;
 vector<string> seqs;
 
 UF* uf;
@@ -30,10 +31,14 @@ void read_file() {
     while (getline(f, line)) {
       string identifier;
 
-      uris.push_back(line.substr(1, strlen(line)));
+      string uri = line.substr(1, strlen(line));
+      uris.push_back(uri);
+      uri_ids.insert(make_pair(uri, index));
 
       getline(f, line);
       seqs.push_back(line);
+
+      index++;
     }
 
     f.close();
@@ -43,28 +48,33 @@ void read_file() {
 }
 
 
+void write_query_file(const string &uri, const string &seq, const string &thread_id) {
+  // TODO
+}
+
+
 void* spin(void *args) {
   struct partition *p = (struct partition*) args;
 
-  int i = 0;
+  for (int uri_idx = p->low; uri_idx < p->high; uri_idx++) {
+    map<string, float> distances;
 
-  for (int first = p->first_partition.first; first < p->first_partition.second; first++) {
-    for (int second = p->second_partition.first; second < p->second_partition.second; second++) {
-      // TODO switch to blast
-      float distance = JD(first, second);
+    write_query_file(uris[uri_idx], seqs[uri_idx], p->thread_id);
 
-      if (distance < DISTANCE_THRESHOLD) {
+    string query_path = "../../blast/db/synbiohubdb/queries/query" + p->thread_id + ".fsa";
+    blast_query(distances, "../../blast/db/synbiohubdb/synbiohub.fsa", query_path);
+
+    // TODO test change locking structure to batch merges
+    for (auto distance : distances) {
+      cout << distance.first << " " << distance.second << endl;
+
+      if (distance.second < DISTANCE_THRESHOLD) {
         pthread_mutex_lock(&uf_mutex);
-        uf->merge(first, second);
+        uf->merge(uri_idx, uri_ids[distance.first]);
         //cout << p->thread_id << " unioned  " << first << " " << second << " distance: " << distance << endl;
         pthread_mutex_unlock(&uf_mutex);
       } else {
         //cout << p->thread_id << " no union " << first << " " << second << " distance: " << distance << endl;
-      }
-
-      i++;
-      if (i % 10000 == 0) {
-        cout << p->thread_id << " iteration " << i << endl;
       }
     }
   }
@@ -98,24 +108,26 @@ int main() {
 
   // create partitions
   vector<struct partition*> partitions;
-  int interval_size = uris.size() / NUM_RANGES;
+  int interval_size = (uris.size() / NUM_WORKERS) + 1;
 
-  for (int first = 0; first < NUM_RANGES; first++) {
-    for (int second = 0; second < NUM_RANGES; second++) {
-      struct partition* p = (struct partition*) malloc(sizeof(struct partition));
+  int uri_id = 0;
 
-      p->first_partition = make_pair(first * interval_size, (first+1) * interval_size);
-      p->second_partition = make_pair(second * interval_size, (second+1) * interval_size);
+  for (int i = 0; i < NUM_WORKERS; i++) {
+    struct partition* p = (struct partition*) malloc(sizeof(struct partition));
 
-      partitions.push_back(p);
-    }
+    p->low = uri_id;
+    uri_id = min(uri_id + interval_size, uris.size());
+    p->high = uri_id;
+
+    p->thread_id = i;
+
+    partitions.push_back(p);
   }
 
   // create threads
   pthread_t threads[NUM_WORKERS];
 
   for (int i = 0; i < NUM_WORKERS; i++) {
-    partitions[i]->thread_id = i;
     int ret = pthread_create(&threads[i], NULL, spin, partitions[i]);
 
     if (ret != 0) {
