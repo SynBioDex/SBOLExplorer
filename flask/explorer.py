f34dc2c1e4cb231f82bbba395f61bc2275b1be8c
Michael Zhang <michael13162@gmail.com>
2018-07-01 14:19:39 -0600

    implemented synbiohub -> explorer API

--- flask/explorer.py
+++ flask/explorer.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
 import re
 
 app = Flask(__name__)
@@ -8,6 +10,20 @@ app = Flask(__name__)
 @app.route('/hello')
 def hello_world():
     return 'Hello, World!'
+
+
+es = Elasticsearch(['http://localhost:9200/'], verify_certs=True)
+index_name = 'part'
+
+def search_es(query):
+    s = Search(using=es, index=index_name).query('multi_match', query=query, fields=['uri', 'display_id', 'name', 'description'])
+    print(s.to_dict())
+    response = s.execute()
+
+    for hit in response:
+        print(hit.meta.score, hit.uri) 
+
+    return response
 
 
 count_response = {"head":{"link":[],"vars":["count"]},"results":{"distinct":False,"ordered":True,"bindings":[{"count":{"type":"typed-literal","datatype":"http://www.w3.org/2001/XMLSchema#integer","value":"0"}}]}}
@@ -30,6 +46,9 @@ def search():
         keywords.append(keyword)
 
     print(keywords)
+
+    es_response = search_es(' '.join(keywords))
+    print(es_response)
 
     return jsonify(results_response)
 
