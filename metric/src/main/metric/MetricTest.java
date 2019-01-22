d25cc73a23c61c895a885e065472eadd1154ff89
Chris Myers <myers@ece.utah.edu>
2019-01-22 11:41:43 -0700

    Updated metric code

--- metric/src/main/metric/MetricTest.java
+++ metric/src/main/metric/MetricTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import org.sbolstandard.core2.Component;
@@ -19,51 +20,59 @@ import com.google.common.collect.Lists;
 public class MetricTest {
 
 	private static final int NUM_PARTS = 200;
-	private static final String SYNBIOHUB_BACKEND_URL = "http://localhost:7777";
+	private static final String SYNBIOHUB_BACKEND_URL = "https://dev.synbiohub.org";
+	private static final String SYNBIOHUB_BACKEND_URL_NOEX = "https://synbiohub.org";
 	private static final String SYNBIOHUB_URI_PREFIX = "https://synbiohub.org";
 
 	private static final SynBioHubFrontend frontend = new SynBioHubFrontend(SYNBIOHUB_BACKEND_URL,
+			SYNBIOHUB_URI_PREFIX);
+
+	private static final SynBioHubFrontend frontend_noex = new SynBioHubFrontend(SYNBIOHUB_BACKEND_URL_NOEX,
 			SYNBIOHUB_URI_PREFIX);
 
 	public static void main(String[] args) throws Exception {
 		List<ComponentDefinition> engineeredRegions = getEngineeredRegions();
 
 		System.out.println("Getting metrics using SBOLExplorer");
-		ArrayList<Double> sbolExplorerScores = getMetric(NUM_PARTS, engineeredRegions);
+		ArrayList<Double> sbolExplorerScores = getMetric(frontend, NUM_PARTS, engineeredRegions);
 		System.out.println("Done getting metrics using SBOLExplorer");
 
-		System.in.read();
+		//System.in.read();
 
 		System.out.println("Getting metrics without using SBOLExplorer");
-		ArrayList<Double> noSBOLExplorerScores = getMetric(NUM_PARTS, engineeredRegions);
+		ArrayList<Double> noSBOLExplorerScores = getMetric(frontend_noex, NUM_PARTS, engineeredRegions);
 		System.out.println("Done getting metrics without using SBOLExplorer");
 
 		analyzeScores(sbolExplorerScores, noSBOLExplorerScores);
 	}
 
 	private static List<ComponentDefinition> getEngineeredRegions() throws IOException, Exception {
+		HashSet<URI> collections = new HashSet<URI>();
+		//collections.add(URI.create("https://synbiohub.org/public/igem/igem_collection/1"));
 		SynBioHubQuery query = new SynBioHubQuery(frontend,
 				new HashSet<URI>(Arrays.asList(SequenceOntology.ENGINEERED_REGION)), new HashSet<URI>(),
-				new HashSet<URI>(), null, "ComponentDefinition");
+				collections, null, "ComponentDefinition");
 
 		ArrayList<IdentifiedMetadata> metadatas = query.execute();
 		ArrayList<ComponentDefinition> cds = new ArrayList<>();
 
 		System.out.println(metadatas.size());
 		for (IdentifiedMetadata metadata : metadatas) {
+			System.out.println("Fetching: "+metadata.getUri() + " (" + cds.size() + ")");
 			try {
 				SBOLDocument doc = frontend.getSBOL(URI.create(metadata.getUri()));
 				ComponentDefinition cd = doc.getRootComponentDefinitions().iterator().next();
 
 				if (!cd.getComponents().isEmpty()) {
 					cds.add(cd);
-					System.out.println("got: " + cds.size());
 
 					if (cds.size() == NUM_PARTS) {
 						break;
 					}
 				}
 			} catch (SynBioHubException e) {
+				System.out.println("URL:"+frontend.getBackendUrl());
+				System.out.println("URI:"+metadata.getUri());
 				e.printStackTrace();
 			}
 		}
@@ -71,43 +80,52 @@ public class MetricTest {
 		return cds;
 	}
 
-	private static ArrayList<Double> getMetric(int numParts, List<ComponentDefinition> engineeredRegions)
+	private static ArrayList<Double> getMetric(SynBioHubFrontend frontend, 
+			int numParts, List<ComponentDefinition> engineeredRegions)
 			throws Exception {
-		ArrayList<Double> scores = new ArrayList<>();
-
+//		ArrayList<Double> scores = new ArrayList<>();
+		ArrayList<Double> indices = new ArrayList<>();
+		int parts = 0;
+		int total = 0;
+		int notFound = 0;
+		
 		for (ComponentDefinition cd : engineeredRegions) {
+			parts++;
 			List<String> results = null;
 			try {
+				HashSet<URI> collections = new HashSet<URI>();
+//				collections.add(URI.create("https://synbiohub.org/public/igem/igem_collection/1"));
 				SynBioHubQuery query = new SynBioHubQuery(frontend, new HashSet<URI>(), new HashSet<URI>(),
-						new HashSet<URI>(), cd.getDescription(), "ComponentDefinition");
+						collections, cd.getDescription(), "ComponentDefinition");
 				results = Lists.transform(query.execute(), metadata -> metadata.getUri());
 			} catch (Exception e) {
 				System.out.println("Exception: skipping " + cd.getDisplayId());
 				continue;
 			}
-
-			ArrayList<Double> indices = new ArrayList<>();
+//			ArrayList<Double> indices = new ArrayList<>();
 
 			for (Component child : cd.getComponents()) {
 				int index = results.indexOf(child.getDefinition().getIdentity().toString());
-
+				total++;
 				if (index >= 0) {
 					indices.add((double) index);
 				} else {
-					indices.add(10000.0);
+					notFound++;
+					//indices.add(10000.0);
 				}
 			}
-
-			scores.add(average(indices));
-
+			//if (indices.size()>0) {
+				//scores.add(average(indices));
+			//}
 			System.out.print('|');
 
-			if (scores.size() == numParts) {
+			if (parts == numParts) {
 				break;
 			}
 		}
-
-		return scores;
+		System.out.println("");
+		System.out.println(notFound+" not found out of "+total);
+		return indices;
 	}
 
 	private static void analyzeScores(ArrayList<Double> sbolExplorerScores, ArrayList<Double> noSBOLExplorerScores) {
