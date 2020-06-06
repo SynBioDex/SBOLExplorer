af6d710fef1319d1921186c4b10504a7a2d87b24
Eric <ejyu99@gmail.com>
2020-06-06 15:54:48 -0600

    Add support for file upload when sequence searching

--- flask/sequencesearch.py
+++ flask/sequencesearch.py
@@ -3,7 +3,10 @@ import subprocess
 import utils
 import query
 import cluster
+import search
 from sys import platform
+import base64
+import tempfile
 
 
 
@@ -18,26 +21,26 @@ else:
 globalFlags = {'maxaccepts': '50', 'id': '0.8', 'iddef': '2', 'maxrejects': '0', 'maxseqlength': '5000', 'minseqlength': '20'}
 exactFlags = {}
 
-def write_to_fasta(sequence):
-    f = open('usearch/searchsequence.fsa', 'w')
-    f.write('>sequence_to_search\n')
-    f.write('%s\n' % sequence)
-    f.close()
-
+def write_to_temp(sequence):
+    temp = tempfile.NamedTemporaryFile(suffix=".fsa",delete=False)
+    with open(temp.name, 'w') as f:
+        f.write('>sequence_to_search\n')
+        f.write('%s\n' % sequence)
+    return temp.name
 
 # pass in the sequence to this function, replace searchsequence.fsa with the query sequence
-def run_vsearch_global():
+def run_vsearch_global(fileName):
     # setting maxaccepts to 0 disables the limit (searches for all possible matches)
-    args = [vsearch_binary_filename, '--usearch_global', 'usearch/searchsequence.fsa', '--db', 'usearch/sequences.fsa', '--dbmatched', 'usearch/sbsearch_results.fsa', '--uc', 'usearch/sbsearch_uctable.uc', '--uc_allhits',]
+    args = [vsearch_binary_filename, '--usearch_global', fileName, '--db', 'usearch/sequences.fsa','--uc', fileName[:-4] + '.uc', '--uc_allhits',]
     args = append_flags_to_args(args, globalFlags)
     popen = subprocess.Popen(args, stdout=subprocess.PIPE)
     popen.wait()
     output = popen.stdout.read()
     print(output)
 
-def run_vsearch_exact():
+def run_vsearch_exact(fileName):
     # setting maxaccepts to 0 disables the limit (searches for all possible matches)
-    args = [vsearch_binary_filename, '--search_exact', 'usearch/searchsequence.fsa', '--db', 'usearch/sequences.fsa', '--dbmatched', 'usearch/sbsearch_results.fsa', '--uc', 'usearch/sbsearch_uctable.uc', '--uc_allhits']
+    args = [vsearch_binary_filename, '--search_exact', fileName, '--db', 'usearch/sequences.fsa','--uc', fileName[:-4] + '.uc', '--uc_allhits']
     args = append_flags_to_args(args, exactFlags)
     popen = subprocess.Popen(args, stdout=subprocess.PIPE)
     popen.wait()
@@ -62,16 +65,16 @@ def add_exact_flags(userFlags):
             exactFlags[flag] = userFlags[flag]
 
 
-def sequence_search(userFlags):
+def sequence_search(userFlags, fileName):
     utils.log('Starting sequence search')
 
     if "search_exact" in userFlags:
         add_exact_flags(userFlags)
-        run_vsearch_exact()
+        run_vsearch_exact(fileName)
     else:
         add_global_flags(userFlags)
-        run_vsearch_global()
+        run_vsearch_global(fileName)
     utils.log('Sequence search complete')
 
-    return cluster.uclust2uris()
+    return cluster.uclust2uris(fileName[:-4] + '.uc')
 
