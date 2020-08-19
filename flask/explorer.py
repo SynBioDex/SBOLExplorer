8423c3194820bda6e8b7467da3cac7a7e1dbdff8
Eric <ejyu99@gmail.com>
2020-08-18 18:07:24 -0600

    Update explorer.py

--- flask/explorer.py
+++ flask/explorer.py
@@ -33,8 +33,8 @@ def handle_error(e):
 @app.before_first_request
 def startup():
     utils.log('SBOLExplorer started :)')
-    es_indices = utils.get_es().indices.get(index='*,-localhost*,-.kibana')
-    if (len(es_indices) == 0):
+    
+    if utils.get_es().indices.exists(index=utils.get_config()['elasticsearch_index_name']) is False:
         utils.log('Index not found, creating new index.')
         requests.get(request.url_root + '/update')
 
@@ -134,7 +134,7 @@ def incremental_remove_collection():
 def sparql_search_endpoint():
     try:
         # make sure index is built, or throw exception
-        if utils.get_es().indices.exists(index='*,-localhost*,-.kibana') is False or utils.get_es().cat.indices(format='json')[0]['health'] is 'red':
+        if utils.get_es().indices.exists(index=utils.get_config()['elasticsearch_index_name']) is False or utils.get_es().cat.indices(format='json')[0]['health'] is 'red':
             abort(503, 'Elasticsearch is not working or the index does not exist.')
 
         sparql_query = request.args.get('query')
@@ -150,7 +150,7 @@ def sparql_search_endpoint():
 @app.route('/search', methods=['GET'])
 def search_by_string():
     try:
-        if utils.get_es().indices.exists(index='*,-localhost*,-.kibana') is False or utils.get_es().cat.indices(format='json')[0]['health'] is 'red':
+        if utils.get_es().indices.exists(index=utils.get_config()['elasticsearch_index_name']) is False or utils.get_es().cat.indices(format='json')[0]['health'] is 'red':
             abort(503, 'Elasticsearch is not working or the index does not exist.')
 
         query = request.args.get('query')
